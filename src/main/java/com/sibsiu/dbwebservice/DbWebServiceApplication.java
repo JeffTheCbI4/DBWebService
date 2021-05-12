9457f28144a07c5b7e2e503f10f219818438e7af
Kirill <sergushinkirill@gmail.com>
2021-05-12 17:56:54 +0700

    Initial commit

---
+++ src/main/java/com/sibsiu/dbwebservice/DbWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sibsiu.dbwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbWebServiceApplication.class, args);
+	}
+
+}
